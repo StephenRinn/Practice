444260a9b3d968dde9a116958d0f28708d8951bb
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-02-07 11:56:19 -0600

    Time: 4 ms (40.98%), Space: 42.5 MB (7.65%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -2,13 +2,14 @@ class Solution {
     public char findTheDifference(String s, String t) {
         
         int[] alpha = new int[26];
-        int asciiOffset = 97;
+        int asciiOffset = 97; //Converting from ascii to array location
 
-        
+        // Find number of letters in first string for comparison
         for(int i = 0; i < s.length(); i++){
             alpha[s.charAt(i) - asciiOffset]++;
         }
         
+        // Iterate down until the negative is found
         for(int i = 0; i < t.length(); i++){
             alpha[(t.charAt(i) - asciiOffset)]--;
             if(alpha[t.charAt(i) - asciiOffset]<0){
