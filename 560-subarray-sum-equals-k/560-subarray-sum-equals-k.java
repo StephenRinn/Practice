ca2502fbba29b1129af8f4b5f6a11acab9fb8d96
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-02-10 09:04:18 -0600

    Time: 47 ms (34.67%), Space: 61.5 MB (11.89%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,3 +1,6 @@
+// Study (failed on size using array - only considered small fast implementations because of test inputs.)
+
+// Traverses once and creates a N sized map - Really clever solution remember
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
@@ -6,6 +9,9 @@ class Solution {
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
+            
+            // check for subarray based on removing the target from the sum
+            // then position of (sum - k) + 1 is start of array that sums to target
             if (map.containsKey(sum - k))
                 count += map.get(sum - k);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
