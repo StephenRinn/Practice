8c603e736f3b549ae54cbd746ceac2c392b768e1
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-02-10 11:05:49 -0600

    Time: 8 ms (74.33%), Space: 48 MB (16.34%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,21 +1,18 @@
 import java.util.HashSet;
+import java.util.HashMap;
 
 class Solution {
     public int findPairs(int[] nums, int k) {
 
         HashSet<Integer> exists = new HashSet<Integer>();
-        HashSet<Integer> dupe = new HashSet<Integer>();
-        Integer previous = 0;
         int count = 0;
         
+        // Edge case of k = 0
         if(k == 0){
+            HashMap<Integer,Integer> dupe = new HashMap<Integer,Integer>();
             for(int i = 0; i < nums.length; i++){
-                if(exists.contains(nums[i]) && !dupe.contains(nums[i])){
-                    count++;
-                    dupe.add(nums[i]);
-                }else if(!exists.contains(nums[i])){
-                    exists.add(nums[i]);
-                }
+                dupe.put(nums[i],dupe.getOrDefault(nums[i],0) + 1);
+                if(dupe.get(nums[i]) == 2){count++;}
             }
             return count;
         }
@@ -25,7 +22,7 @@ class Solution {
             exists.add(nums[i]);
         }
         
-
+        // Check for k difference (one directional to get rid of removal issues)
         for(Integer i: exists){
             if(exists.contains(i+k)){
                 count++;
