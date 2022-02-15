e51240e6778829339cf9c8c77487d121e6c01892
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-02-15 09:09:47 -0600

    Time: 1 ms (95.92%), Space: 42.3 MB (68.78%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -3,21 +3,12 @@ import java.util.HashSet;
 class Solution {
     public int singleNumber(int[] nums) {
         
-        HashSet<Integer> NumberCount = new HashSet<Integer>();
-        int singleDigit = 0;
+        int output = 0;
         
-        // Check hashset and delete it if it exists
-        for(int i : nums){
-            if(!NumberCount.add(i)){
-                NumberCount.remove(i);;
-            }
+        for(int i: nums){
+            output ^= i;
         }
         
-        // find only remaining solo number
-        for(int i : NumberCount){
-            singleDigit = i;
-        }
-        
-        return singleDigit;
+        return output;
     }
 }
