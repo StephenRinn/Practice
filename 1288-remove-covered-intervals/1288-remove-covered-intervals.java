0d6eba5c4adeb4ce70a09af1de158d61ef41b7a9
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-02-19 20:17:49 -0600

    Time: 6 ms (71.14%), Space: 46.7 MB (11.39%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        return removeCoveredIntervals2(intervals);
+    }
+    
+     public int removeCoveredIntervals2(int[][] A) {
+        int res = 0, right = 0;
+        Arrays.sort(A, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
+        for (int[] v : A) {
+            if (v[1] > right) {
+                ++res;
+                right = v[1];
+            }
+        }
+        return res;
+    }
+}
