151f0f56c966449c08dc47d48fbb2b72d052a912
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-02-21 22:37:03 -0600

    Time: 1 ms (99.69%), Space: 42.6 MB (25.66%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.java
@@ -3,10 +3,13 @@ class Solution {
         int baseNum = 1;
         int sum = 0;
         int offset = 64;
+        
+        // Set up base 26 conversion with ascii offset
         for(int i = columnTitle.length() - 1; i >=0; i--){
             sum += (columnTitle.charAt(i) - offset) * baseNum;
             baseNum = baseNum * 26;
         }
+        
         return sum;
     }
 }
