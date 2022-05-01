eb5e55dd6e6fcd84737a7515b031b781099b0551
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-05-01 14:04:37 -0500

    Time: 1729 ms (5.03%), Space: 69.9 MB (6.76%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int[] canReach = new int[nums.length];
+        canReach[0] = 1;
+        int jump = 0;
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            if(canReach[i] == 1){
+                jump = nums[i];
+                for(int j = 0; j <= jump; j++){
+                    if(j+i < (nums.length)){
+                        canReach[j + i] = 1;
+                    } 
+                }
+            }
+        }
+        
+        return canReach[nums.length-1] == 1;
+    }
+}
