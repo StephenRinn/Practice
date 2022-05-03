c9b18ae4219759bf92d33a4261c9a9f394f28f36
StephenRinn <69868288+StephenRinn@users.noreply.github.com>
2022-05-03 17:09:28 -0500

    Time: 0 ms (100.00%), Space: 44.4 MB (22.81%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -4,10 +4,16 @@ class Solution {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
-    /* Overload for recursion */
+    /* Overload for recursion   */
     public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        
+        /*  If there are no branches or leaves it must be balanced   */
         if (root == null) {return true;}
+        
+        /*  Check values against value caps set by branches above    */
         if (root.val >= maxVal || root.val <= minVal){return false;}
+        
+        /*  Use recursion to find leaves    */
         return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
